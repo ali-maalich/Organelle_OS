dbf56969fc7c282e275c87cd5865359c44f40c29
ali maalich <48730572+ali-maalich@users.noreply.github.com>
2023-07-04 21:05:39 +0200

    Update 0.sh

--- scripts/0.sh
+++ scripts/0.sh
@@ -6,6 +6,8 @@ echo "Enter [root] at the prompt that appears"
 sudo passwd root
 sudo raspi-config nonint do_hostname barbonelle
 
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
 echo "Step #0 finished."
 echo "The Raspberry will restart - press a button"
 read
