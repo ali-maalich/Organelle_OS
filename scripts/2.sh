ba85312a7630cb7d9f4763271dcc1ff842bf93c7
ali maalich <48730572+ali-maalich@users.noreply.github.com>
2023-07-04 20:51:30 +0200

    Update 2.sh

--- scripts/2.sh
+++ scripts/2.sh
@@ -8,7 +8,6 @@ sudo mkdir /sdcard
 sudo chown music:music /sdcard
 sudo mkdir /usbdrive
 sudo chown music:music /usbdrive
-sudo echo "/dev/mmcblk0p3 /sdcard  ext4 defaults,noatime 0 0" >> /etc/fstab
 
 echo "#2 terminato."
 echo "Premere un tasto per riavviare"
